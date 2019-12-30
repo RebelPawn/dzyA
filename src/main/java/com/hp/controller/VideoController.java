184c6a87baa6d987963f1bb6b7107c6630559225
dzy <913430528@qq.com>
2019-12-30 18:57:16 +0800

    文件处理

--- src/main/java/com/hp/controller/VideoController.java
+++ src/main/java/com/hp/controller/VideoController.java
@@ -3,6 +3,7 @@ package com.hp.controller;
 import com.util.FileUtil;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 public class VideoController {
@@ -13,7 +14,7 @@ public class VideoController {
         FileUtil fileUtil=new FileUtil();
         fileUtil.renameFile(path);
         File[] files=fileUtil.getFileList(path);
-        List<String[]> infos=null;
+        List<String[]> infos=new ArrayList<>();
         String[] info={"",""};
         for(int index=0;index<files.length;index++){
 
@@ -21,25 +22,26 @@ public class VideoController {
             int length=fileUtil.videoTime(path+fileName);
             info[0]=fileName;
             info[1]=String.valueOf(length);
-
             infos.add(index,info);
         }
-
 
         return  infos;
     }
 
     public void add(List<String[]> videoInfos){
 
+        for (String[] info:videoInfos){
+            int num=Integer.parseInt(info[0].split(" ")[0]);
+            String fileName=info[0].split(" ")[1];
+            System.out.println(num+"-----"+fileName+"----"+info[1]);
+        }
     }
     public static void main(String[] args) {
         VideoController vc=new VideoController();
 
         String path="E:\\BaiduNetdiskDownload\\尚硅谷Java数据结构和算法\\test\\";
         List<String[]> infos=vc.getList(path);
-        for (String[] info:infos){
-            System.out.println(info[0]+"----"+info[1]);
-        }
-        //vc.add(infos);
+
+        vc.add(infos);
     }
 }
